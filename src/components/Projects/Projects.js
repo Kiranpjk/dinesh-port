fd7df282ea096a541381a369f5c884b55ba12d72
Kiranpjk <142809592+Kiranpjk@users.noreply.github.com>
2025-04-30 00:03:02 +0530

    Update Projects.js

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -29,27 +29,27 @@ of confirmed cases, recoveries, and deaths across different regions.Utilized Tab
             />
           </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Bookstore}
-              isBlog={false}
-              title="Bookstore"
-              description="A dynamic web application that allows users to browse, search, and purchase books online. Developed with a clean and intuitive interface using React.js for the frontend and Firebase (or Laravel—let me know if that’s the case) for backend functionalities like authentication, data storage, and order management. The platform supports real-time updates, shopping cart functionality, and secure user authentication."
-              ghLink="https://github.com/eshwarbattuwar/OnlineBookStore"
-              // demoLink="https://blogs.soumya-jit.tech/"
-            />
-          </Col>
+          // <Col md={4} className="project-card">
+          //   <ProjectCard
+          //     imgPath={Bookstore}
+          //     isBlog={false}
+          //     title="Bookstore"
+          //     description="A dynamic web application that allows users to browse, search, and purchase books online. Developed with a clean and intuitive interface using React.js for the frontend and Firebase (or Laravel—let me know if that’s the case) for backend functionalities like authentication, data storage, and order management. The platform supports real-time updates, shopping cart functionality, and secure user authentication."
+          //     ghLink="https://github.com/eshwarbattuwar/OnlineBookStore"
+          //     // demoLink="https://blogs.soumya-jit.tech/"
+          //   />
+          // </Col>
 
-          <Col md={4} className="project-card">
-            <ProjectCard
-              imgPath={Ecommerce}
-              isBlog={false}
-              title="E-Commerce"
-              description="A full-featured online shopping platform developed to provide users with a seamless buying experience. Designed using React.js for the frontend and Laravel for the backend, the application offers a visually appealing interface, smooth navigation, and robust functionality. It includes product browsing, user authentication, cart management, order processing, and admin controls for inventory and order handling."
-              ghLink="https://github.com/eshwarbattuwar/E-Commerce"
-              // demoLink="https://editor.soumya-jit.tech/"              
-            />
-          </Col>
+          // <Col md={4} className="project-card">
+          //   <ProjectCard
+          //     imgPath={Ecommerce}
+          //     isBlog={false}
+          //     title="E-Commerce"
+          //     description="A full-featured online shopping platform developed to provide users with a seamless buying experience. Designed using React.js for the frontend and Laravel for the backend, the application offers a visually appealing interface, smooth navigation, and robust functionality. It includes product browsing, user authentication, cart management, order processing, and admin controls for inventory and order handling."
+          //     ghLink="https://github.com/eshwarbattuwar/E-Commerce"
+          //     // demoLink="https://editor.soumya-jit.tech/"              
+          //   />
+          // </Col>
 
           
           
