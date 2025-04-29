6120d9623afa5859ec00ad5d5fa02242b445392c
Kiranpjk <142809592+Kiranpjk@users.noreply.github.com>
2025-04-30 00:02:19 +0530

    Update Certifications.js

--- src/components/Certifications/Certifications.js
+++ src/components/Certifications/Certifications.js
@@ -4,7 +4,7 @@ import CertificationsCard from "./CerticationsCards";
 import Particle from "../Particle";
 import CipherSchools from "../../Certifications/PHP.png";
 import PHP from "../../Certifications/CipherSchools.png";
-import DSA from "../../Certifications/DSA JPG.jpg";
+import DSA from "../../Certifications/DSA JPG.png";
 
 function Certifications() {
   return (
